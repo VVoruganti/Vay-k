795b0401104bd5f2b7ff7de0fe912caa9ae09805
nkuttua3 <91503766+nkuttua3@users.noreply.github.com>
2022-04-15 21:45:52 -0400

    Update script.js

--- extension/src/script.js
+++ extension/src/script.js
@@ -1,13 +1,13 @@
-function openTab(evt, tabName) {
-    var i, x, tablinks;
-    x = document.getElementsByClassName("tabContent");
+document.addEventListener('DOMContentLoaded', function() {
+  var link = document.getElementById('link');
+  // onClick's logic below:
+  link.addEventListener('click', function openTab(cityName) {
+    var i;
+    var x = document.getElementsByClassName("city");
     for (i = 0; i < x.length; i++) {
-      x[i].style.display = "none";
+      x[i].style.display = "none";  
     }
-    tablinks = document.getElementsByClassName("tablink");
-    for (i = 0; i < x.length; i++) {
-      tablinks[i].className = tablinks[i].className.replace(" active", "");
-    }
-    document.getElementById(tabName).style.display = "block";
-    evt.currentTarget.className += " active";
-  }
+    document.getElementById(cityName).style.display = "block"; 
+  });
+});
+
