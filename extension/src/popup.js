8564817658efc0b935d1eb49594554b583e51668
nkuttua3 <91503766+nkuttua3@users.noreply.github.com>
2022-04-19 19:29:39 -0400

    Update popup.js

--- extension/src/popup.js
+++ extension/src/popup.js
@@ -1,10 +1,18 @@
 document.addEventListener('DOMContentLoaded', function() {
     var selectedMenu;
-    document.getElementById("tablinkContainer").addEventListener("click", function(e) {
+    document.getElementById("tablinkContainer").addEventListener("click", async function(e) {
       selectedMenu;
       if (selectedMenu) {
         // hide previously selected menu content
         document.getElementById(selectedMenu).classList.add('hidden');
+        if (selectedMenu == "trip") {
+          
+        } else if (selectedMenu == "bookmark") {
+          const res = await fetch(`https://vay-k.herokuapp.com/trips`);
+          const data = await res.json();
+          console.log(data);
+        }
+
       }
       // show currently selected menu content
       selectedMenu = e.target.dataset.link;
@@ -12,14 +20,27 @@ document.addEventListener('DOMContentLoaded', function() {
     });
   });
 
-async function getCurrentTabURL() {
+  async function getCurrentTabURL() {
     const tabs = await chrome.tabs.query({active: true, currentWindow: true})
     let url = tabs[0].url;
     return url;
-}
+  }
 
-async function addBookmark() {
-    fetch(`http://localhost:5000/add/bookmark`, {
+  async function addBookmarkPhoto() {
+    fetch(`https://vay-k.herokuapp.com/add/photo/id`, {
         body: JSON.stringify(await getCurrentTabURL())
     })   
-}
+  }
+
+  async function addBookmarkWebpage() {
+    fetch(`https://vay-k.herokuapp.com/add/webpage/` + id, {
+        body: JSON.stringify(await getCurrentTabURL())
+    })   
+  }
+  
+  // as soon as button on trips tab gets hit, getCurrentTabURL is called
+  document.getElementById("trip-button").addEventListener("click", function() {
+    var url = getCurrentTabURL();
+    window.onclick = getCurrentTabURL();
+    console.log(url);
+  });
